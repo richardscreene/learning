17639a4475ba6b417f1607c6cb3f5e2e706f1750
Richard Screene <rscreene@yahoo.co.uk>
2020-07-30 16:20:44 +0100

    First attempt

---
+++ backend/user/graphql.ts
@@ -0,0 +1,35 @@
+import { buildSchema, GraphQLSchema } from "graphql";
+//import { GraphQLSchema } from "graphql.d.ts";
+import * as user from "./user";
+
+const schema: GraphQLSchema = buildSchema(`
+  enum Role {
+    admin
+  	user
+  }
+
+  type User {
+    userId: ID!
+  	email: String!
+  	name: String!
+  	role: Role!
+  }
+
+  type Query {
+    get(userId: ID!): User
+    list(skip: Int, limit: Int): [User!]
+  }
+`);
+
+const rootValue: object = {
+	//curl -X POST -H "Content-Type: application/json" -d '{"query": "{ list(skip: 5,limit:1) { email } }"}' http://localhost:3000/graphql
+	list: ({ skip, limit }: { skip: number; limit: number }) => {
+		return user.list(skip, limit);
+	},
+	get: ({ userId }: { userId: string }) => {
+    console.log("userId=", userId);
+		return user.retrieve(userId);
+	}
+};
+
+export { schema, rootValue };
